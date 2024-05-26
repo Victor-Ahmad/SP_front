62394e82d7ca8c1f2f3fa8e12b98275a0d53c3ac
Victor-Ahmad <victor.ahmad.mail@gmail.com>
2024-05-26 19:20:42 +0300

    .

---
+++ resources/views/testtesttest.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Iframe Example</title>
+</head>
+
+<body>
+    <h1>Contact Us Form</h1>
+    <iframe src="https://dev.emdadatalatta.com/contactUsFromWebsite" width="100%" height="700" frameborder="0">
+        Your browser does not support iframes.
+    </iframe>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -66,7 +66,9 @@ Route::middleware([Localization::class])->group(function () {
 
 
 
-
+Route::get('/testtesttest',function(){
+    return view('testtesttest');
+})
 
 
 
